6e410284df5ac8ee9144a42421640cf5535526b0
Necromancer <necromancerr@users.noreply.github.com>
2019-07-16 00:33:58 +0200

    determine download application with command line options

--- shell/k8s/pod/lazy-shell.sh
+++ shell/k8s/pod/lazy-shell.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
+
+if [[ -z "${DOWNLOAD-}" ]]
+then
+	which curl >/dev/null && export DOWNLOAD="curl -sL" && rc=0 || rc=$?
+	if [ "$rc" -ne 0 ]
+	then
+		which wget >/dev/null && export DOWNLOAD="wget -qO-" && rc=0 || rc=$?
+	fi
+	[ "$rc" -ne 0 ] && echo "ERROR: neither curl nor wget installed !" && exit 1
+fi
+
 export SH=`which bash`
 if [[ ! -z "${SH-}" ]]
 then
@@ -8,4 +19,5 @@ else
 	export IS_BASH=0
 	export SH="/bin/sh"
 	sh -o errexit -o nounset "$@"
+fi
 fi
